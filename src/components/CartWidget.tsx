b0bd2649bd4fb0d6bcf408517d87fe0809f56b6a
Santiago Monedero <santiagomonedero122@gmail.com>
2023-02-05 15:05:55 -0300

    Other changes

--- src/components/CartWidget.tsx
+++ src/components/CartWidget.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 export const CartWidget = () => {
+	const [alertIsVisible, setAlertIsVisible] = useState(false);
+
 	return (
-		<div>
+		<div onClick={() => setAlertIsVisible(!alertIsVisible)}>
 			<ShoppingCartIcon fontSize="large" />
 			<h4>15</h4>
+
+			{alertIsVisible && (
+				<div
+					style={{
+						position: 'absolute',
+						cursor: 'pointer',
+						top: '64px',
+						right: '0',
+						backgroundColor: '#16283b',
+						marginRight: '15px',
+						padding: '10px',
+						borderRadius: '5px',
+						boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+					}}
+				>
+					<p style={{ color: '#ffffffX' }}>Tiene 15 cosas agregadas al carrito</p>
+				</div>
+			)}
 		</div>
 	);
 };
